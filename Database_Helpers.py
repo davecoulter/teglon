fc8eb5564435f32a74405c6bdda83777b213ab5a
davecoulter <davecoulter@gmail.com>
2019-05-21 17:10:48 -0700

    Refactoring db connections

--- Database_Helpers.py
+++ Database_Helpers.py
@@ -1,12 +1,26 @@
 import mysql.connector
 from shapely.geometry import Point
 import pprint
+import os
+from configparser import RawConfigParser
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+configFile = os.path.join(__location__, 'Settings.ini')
+
+config = RawConfigParser()
+config.read(configFile)
+
+
+db_name = config.get('database', 'DATABASE_NAME')
+db_user = config.get('database', 'DATABASE_USER')
+db_pwd = config.get('database', 'DATABASE_PASSWORD')
+db_host = config.get('database', 'DATABASE_HOST')
+db_port = config.get('database', 'DATABASE_PORT')
 
 def QueryDB(query, port='3306', query_data=None):
 	results = []
-	
-	# cnx = mysql.connector.connect(user='prospecktor', password='4Pr0spi3!', host='127.0.0.1', port=port, database='Prospecktor')
-	cnx = mysql.connector.connect(user='prospecktor', password='4Pr0spi3!', host='host.docker.internal', port=port, database='Prospecktor')
+
+	cnx = mysql.connector.connect(user=db_user, password=db_pwd, host=db_host, port=db_port, database=db_name)
 
 	cursor = cnx.cursor()
 	
